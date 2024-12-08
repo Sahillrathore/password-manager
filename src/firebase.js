b70a047b787728f6f40f70a62df145546389eb55
Sahillrathore <sahil921031@gmail.com>
2024-12-08 17:42:00 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBKW-K3I3zjEUAstOld7IyJtKRZyQ94QI8",
+    authDomain: "passmanager-65055.firebaseapp.com",
+    projectId: "passmanager-65055",
+    storageBucket: "passmanager-65055.firebasestorage.app",
+    messagingSenderId: "1085935134355",
+    appId: "1:1085935134355:web:2df73186330cee19224f66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
