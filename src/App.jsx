739c395c3963504adf35036f5240b073fdf7aca3
Sahillrathore <sahil921031@gmail.com>
2025-01-18 12:32:12 +0530

    fix folder and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Homepage from './components/homepage/Homepage'
-import Passmanager from './components/manager/Passmanager'
+import Navbar from './components/Navbar'
+import Passmanager from './pages/Passmanager'
+import Homepage from './components/Homepage'
 
 const App = () => {
   return (
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Hero from '../herosection/Hero'
-import Features from '../features/Features'
-import Banner from '../ctabanner/Banner'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Banner from '../components/Banner'
 
 const Homepage = () => {
   return (
