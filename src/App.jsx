bd514a7470785f12dacdc9213e0b68911deb3484
Sahillrathore <sahil921031@gmail.com>
2025-01-20 16:42:27 +0530

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Passmanager from './pages/Passmanager'
 import Homepage from './components/Homepage'
 import SignUp from './pages/Signup'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
         <Routes>
           <Route path='/' element={<Homepage/>} />
-          <Route path='/managers' element={<Passmanager />} />
+          <Route path='/managers' element={<ProtectedRoute> <Passmanager /> </ProtectedRoute>} />
           <Route path='/signup' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useUserContext } from '../context/userContex'
+import { useNavigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useUserContext();
+  console.log(user);
+  
+  const navigate = useNavigate()
+
+  console.log(user);
+  
+  useEffect(()=> {
+    if (!user) {
+      navigate("/signup")
+    }
+
+  },[user])
+  return (
+    children
+  )
+}
+
+export default ProtectedRoute
