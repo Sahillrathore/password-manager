e73c6789a4dc3248ed1dd123f527fd83c18ab30a
Sahillrathore <sahil921031@gmail.com>
2025-02-09 22:50:18 +0530

    pass reset

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { useUserContext } from '../context/userContex';
 import { useNavigate } from 'react-router-dom';
@@ -10,8 +10,11 @@ const Login = ({ authMethod, setAuthMethod }) => {
         email: '',
         password: ''
     });
-    const [error, setError] = useState(null);
+
     const { user, setUser } = useUserContext();
+    const [error, setError] = useState(null);
+    const [email, setEmail] = useState("");
+    const [passReset, setPassReset] = useState(false);
 
     const navigate = useNavigate();
 
@@ -19,7 +22,7 @@ const Login = ({ authMethod, setAuthMethod }) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     };
-    
+
     const handleLogin = async (e) => {
         e.preventDefault();
         setError(null);
@@ -53,60 +56,100 @@ const Login = ({ authMethod, setAuthMethod }) => {
         }
     };
 
-    
+    const handlePasswordReset = async (email) => {
+        try {
+            await sendPasswordResetEmail(auth, email);
+            alert("Password reset email sent! Check your inbox.");
+        } catch (error) {
+            console.error("Error sending password reset email:", error.message);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handlePasswordReset(email);
+    };
+
     return (
-        <div className="w-full md:w-1/2 p-8 min-h-[595px] max-h-[595px] ">
-            <h2 className="text-3xl font-bold text-gray-800 mb-3">Log in</h2>
-            <p className="text-gray-500 mb-6 text-sm">Login to your account in seconds</p>
+        <>
+            {
+                !passReset ?
 
-            {error && <p className='text-sm text-red-500'>{error}</p>}
-            
-            <form className="flex flex-col gap-3 mt-2">
+                    <div className="w-full md:w-1/2 p-8 min-h-[595px] max-h-[595px] ">
+                        <h2 className="text-3xl font-bold text-gray-800 mb-3">Log in</h2>
+                        <p className="text-gray-500 mb-6 text-sm">Login to your account in seconds</p>
 
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                        Email Address:
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        name='email'
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-purple-500 focus:outline-none"
-                        placeholder="Enter your email"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Create Password:
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name='password'
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-purple-500 focus:outline-none"
-                        placeholder="Enter your password"
-                    />
-                </div>
+                        {error && <p className='text-sm text-red-500'>{error}</p>}
 
-                <button
-                    type="submit"
-                    className="w-full bg-purple-600 text-white py-2 rounded-md text-lg hover:bg-purple-700 transition duration-200 mt-[9.5rem]"
-                    onClick={handleLogin}
-                >
-                    Login
-                </button>
-            </form>
-            <p className="text-gray-600 text-sm mt-4 text-center">
-                Don't have an account?{" "}
-                <a className="text-purple-600 hover:underline cursor-pointer"
-                    onClick={() => { setAuthMethod('signup') }}
-                >
-                    Signup
-                </a>
-            </p>
-        </div>
+                        <form className="flex flex-col gap-3 mt-2">
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                    Email Address:
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name='email'
+                                    onChange={handleChange}
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-purple-500 focus:outline-none"
+                                    placeholder="Enter your email"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                    Create Password:
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name='password'
+                                    onChange={handleChange}
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-purple-500 focus:outline-none"
+                                    placeholder="Enter your password"
+                                />
+                            </div>
+
+                            <div className='-mt-5 text-sm text-gray-700 pl-0.5'
+                            onClick={()=>setPassReset(!passReset)}
+                            >
+                                <button className='underline'>Forgot Password</button>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="w-full bg-purple-600 text-white py-2 rounded-md text-lg hover:bg-purple-700 transition duration-200 mt-[9.5rem]"
+                                onClick={handleLogin}
+                            >
+                                Login
+                            </button>
+                        </form>
+                        <p className="text-gray-600 text-sm mt-4 text-center">
+                            Don't have an account?{" "}
+                            <a className="text-purple-600 hover:underline cursor-pointer"
+                                onClick={() => { setAuthMethod('signup') }}
+                            >
+                                Signup
+                            </a>
+                        </p>
+                    </div>
+
+                    :
+                    <form onSubmit={handleSubmit} className="flex justify-center gap-4 flex-col w-full md:w-1/2 p-8 min-h-[595px] max-h-[595px] ">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            className="border p-2"
+                        />
+                        <button type="submit" className="bg-blue-500 text-white p-2 ">
+                            Reset Password
+                        </button>
+                    </form>
+            }
+        </>
     )
 }
 
---
+++ src/pages/Pagenotfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pagenotfound = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+        <div className='text-4xl font-bold'>Page Not Found</div>
+    </div>
+  )
+}
+
+export default Pagenotfound
