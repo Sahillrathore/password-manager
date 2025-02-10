d1200d4ba0f4721cd79d6cbe6d46ce12e152babc
Sahillrathore <sahil921031@gmail.com>
2025-02-10 17:37:38 +0530

    newcom

---
+++ src/components/GeneratePass.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GeneratePass = () => {
+  return (
+    <div>
+        <div>
+            <input type="text" value='' readOnly />
+
+
+        </div>
+    </div>
+  )
+}
+
+export default GeneratePass
