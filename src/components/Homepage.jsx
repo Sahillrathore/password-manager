954c808c94cc7fac5130d7e501e936ffc06ae93d
Sahillrathore <sahil921031@gmail.com>
2025-02-10 17:37:52 +0530

    home

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,15 +3,17 @@ import Hero from '../components/Hero'
 import Features from '../components/Features'
 import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
+import FeaturesHome from './FeaturesHome'
 
 const Homepage = () => {
   return (
-    <>
+    <div className='bg-[#f3f3f3]'>
         <Navbar/>
         <Hero/>
-        <Features/>
+        {/* <Features/> */}
+        <FeaturesHome/>
         <Banner/>
-    </>
+    </div>
   )
 }
 
